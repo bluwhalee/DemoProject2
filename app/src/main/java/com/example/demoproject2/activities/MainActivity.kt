ecb72fcc13cd23d38f5cf2b91b5321ab4e704333
Bluwhalee <ahmadaliravian@outlook.com>
2023-09-19 18:04:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/demoproject2/activities/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.demoproject2.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.demoproject2.R
+import com.example.demoproject2.adapters.MainVPAdapter
+import com.example.demoproject2.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var pager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var pagerAdapter : MainVPAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    fun init()
+    {
+        pager2 = binding.mainViewPager
+        tabLayout = binding.mainTabLayout
+        pagerAdapter = MainVPAdapter(supportFragmentManager, lifecycle)
+        pager2.adapter = pagerAdapter
+
+
+        TabLayoutMediator(tabLayout,pager2){tab,position ->
+            when(position){
+                1->tab.text = "Maps"
+                2->tab.text = "Category"
+                else->tab.text = "Reviews"
+            }
+        }.attach()
+
+    }
+}
---
+++ app/src/main/java/com/example/demoproject2/adapters/MainVPAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.demoproject2.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.demoproject2.fragments.CategoryFragment
+import com.example.demoproject2.fragments.MapFragment
+import com.example.demoproject2.fragments.ReviewsFragment
+
+class MainVPAdapter(fragmentManager : FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle)  {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            1-> MapFragment()
+            2 -> CategoryFragment()
+            else -> ReviewsFragment()
+        }
+    }
+}
